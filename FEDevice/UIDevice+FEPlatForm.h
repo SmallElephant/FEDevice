d28bd1a9689bf6f476c0e0bfd787629837398b49
FlyElephant <1061731917@qq.com>
2016-01-04 21:16:08 +0800

    初始化提交~

---
+++ FEDevice/UIDevice+FEPlatForm.h
@@ -0,0 +1,15 @@
+//
+//  UIDevice+FEPlatForm.h
+//  FEDevice
+//
+//  Created by keso on 16/1/4.
+//  Copyright © 2016年 FlyElephant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIDevice (FEPlatForm)
+
++ (NSString *)devicePlatForm;
+
+@end

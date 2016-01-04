55e978058c16cb53133284fae9f8b4a29d346c50
FlyElephant <1061731917@qq.com>
2016-01-04 21:00:14 +0800

    Initial Commit

---
+++ FEDevice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FEDevice
+//
+//  Created by keso on 16/1/4.
+//  Copyright © 2016年 FlyElephant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FEDevice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FEDevice
+//
+//  Created by keso on 16/1/4.
+//  Copyright © 2016年 FlyElephant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
